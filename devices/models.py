3d56dbd2f63e7f0640693bcf07bdd63bcda38c9c
d.polissiou <d.polissiou@logicea.com>
2018-05-27 23:36:37 +0300

    removed unnecessary import

--- devices/models.py
+++ devices/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import Group
 from django_netjsonconfig.models import Config
-#from django.contrib.gis.db import models as gismodels
 from djgeojson.fields import PointField, PolygonField
 from django.utils.translation import ugettext as _
 from collectd_rest.models import  GraphGroup
