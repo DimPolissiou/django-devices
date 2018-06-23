d7c29e6b3326ee0c62791ae4b17022942b0be727
DimPolissiou <dimpolissiou@gmail.com>
2018-06-23 21:50:08 +0300

    Fix for new pip

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import os
 from setuptools import find_packages, setup
 
